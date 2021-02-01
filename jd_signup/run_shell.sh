1a4e348af5edb1f6bfd190f1ffa4ab84d063fdaf
bobon <19086312@qq.com>
2021-02-01 21:53:47 +0800

    jd

---
+++ jd_signup/run_shell.sh
@@ -0,0 +1,26 @@
+log_f=$(date -d next-day +"%Y%m%d_%H%M")
+bash /home/myid/jd/jd_signup/api_m_jd_com.sh /home/myid/jd/jd_signup/config /home/myid/jd/jd_signup/api_vender/balibeitian > /home/myid/jd/jd_signup/log/now/balibeitian_${log_f}.log 2>&1 &
+bash /home/myid/jd/jd_signup/api_m_jd_com.sh /home/myid/jd/jd_signup/config /home/myid/jd/jd_signup/api_vender/hanass > /home/myid/jd/jd_signup/log/now/hanass_${log_f}.log 2>&1 &
+bash /home/myid/jd/jd_signup/api_m_jd_com.sh /home/myid/jd/jd_signup/config /home/myid/jd/jd_signup/api_vender/huirenshou > /home/myid/jd/jd_signup/log/now/huirenshou_${log_f}.log 2>&1 &
+bash /home/myid/jd/jd_signup/api_m_jd_com.sh /home/myid/jd/jd_signup/config /home/myid/jd/jd_signup/api_vender/miqilinluntai > /home/myid/jd/jd_signup/log/now/miqilinluntai_${log_f}.log 2>&1 &
+bash /home/myid/jd/jd_signup/api_m_jd_com.sh /home/myid/jd/jd_signup/config /home/myid/jd/jd_signup/api_vender/shuixingjiafang > /home/myid/jd/jd_signup/log/now/shuixingjiafang_${log_f}.log 2>&1 &
+bash /home/myid/jd/jd_signup/api_m_jd_com.sh /home/myid/jd/jd_signup/config /home/myid/jd/jd_signup/api_vender/weizhi3 > /home/myid/jd/jd_signup/log/now/weizhi3_${log_f}.log 2>&1 &
+sleep 1
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_01 /home/myid/jd/jd_signup/vender/beiqinfushi > /home/myid/jd/jd_signup/log/now/config_01_beiqinfushi_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_02 /home/myid/jd/jd_signup/vender/beiqinfushi > /home/myid/jd/jd_signup/log/now/config_02_beiqinfushi_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_03 /home/myid/jd/jd_signup/vender/beiqinfushi > /home/myid/jd/jd_signup/log/now/config_03_beiqinfushi_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_01 /home/myid/jd/jd_signup/vender/daisen > /home/myid/jd/jd_signup/log/now/config_01_daisen_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_02 /home/myid/jd/jd_signup/vender/daisen > /home/myid/jd/jd_signup/log/now/config_02_daisen_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_03 /home/myid/jd/jd_signup/vender/daisen > /home/myid/jd/jd_signup/log/now/config_03_daisen_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_01 /home/myid/jd/jd_signup/vender/hongxing > /home/myid/jd/jd_signup/log/now/config_01_hongxing_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_02 /home/myid/jd/jd_signup/vender/hongxing > /home/myid/jd/jd_signup/log/now/config_02_hongxing_${log_f}.log 2>&1 &
+sleep 0.3
+bash /home/myid/jd/jd_signup/lzkj_isv_signUp.sh /home/myid/jd/jd_signup/config_/config_03 /home/myid/jd/jd_signup/vender/hongxing > /home/myid/jd/jd_signup/log/now/config_03_hongxing_${log_f}.log 2>&1 &
