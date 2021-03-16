b15d4416ec265aa0170efff311a877e5a82acaa4
bobon <19086312@qq.com>
2021-03-16 23:09:16 +0800

    jd

--- jd_signup/search_from_old.sh
+++ jd_signup/search_from_old.sh
@@ -3,5 +3,8 @@ for i in $(cat /home/myid/all_shopid); do
 	#echo "check $i"
 	curl -sS -k https://shop.m.jd.com/?shopId=$i | egrep '/sign/sevenDay/signActivity|/sign/signActivity|/babelDiy/Zeus/' | sed -r -e 's,.*https,https,' -e 's,"\,$,,' -e 's,"$,,' 
 done | tee /home/myid/jd/jd_signup/sign_list
-# | sort | uniq
-#./search.sh sign_list one  ## 一行行执行
+sort sign_list | uniq > sign_list.tmp
+mv -vf sign_list.tmp sign_list
+./search.sh sign_list one  ## 一行行执行
+./search.sh sign_list  ## 修正sign_list后，批量执行
+./sign_res_check.sh  # 检查sign_list是否有签到失败
